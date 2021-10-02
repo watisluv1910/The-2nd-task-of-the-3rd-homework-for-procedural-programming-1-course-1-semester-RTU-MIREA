f5f28d5908eb69373277fe5a5782b1b03c7c10f4
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2021-10-03 01:18:47 +0300

    -Added solution for percentage < 0 (line 38)
    -Cout (line 32) - typo
    -Renaming of variables

--- 2 reworked.cpp
+++ 2 reworked.cpp
@@ -7,85 +7,64 @@
 using namespace std;
 
 // Tasks:
-// проработать алгоритм с отрицательными процентами 
+// проработать алгоритм с отрицательными процентами * done
 // проверка вводимых данных через while
+// доработать r начальное значение
+// доработать step начальное значение
 
 void f2rew() { 
-	double S, n, r, m, error = 0.1, m_guess, step;
-	cout << "Введите величину суммы S:\n";
-	cin >> S;
+	double Sum, years, fractional_percent, monthly_payment, error = 0.1, monthly_payment_guess, step;
+	cout << "Введите величину суммы Sum:\n";
+	cin >> Sum;
 	cout << "Введите величину месячных выплат m:\n";
-	cin >> m;
+	cin >> monthly_payment;
 	cout << "Введите количество лет n:\n";
-	cin >> n;
-	int x = m, deg_of_err = -8;
+	cin >> years;
+	int x = monthly_payment, deg_of_err = -8;
 	while (x > 0)
 	{
 		deg_of_err += 1; // degree of e in error
 		x /= 10;
 	}
 	error = pow(10, (deg_of_err)); // margin of error (depends on number of digits of m)
-	if (m > S) // checking the availability to pay arrears before interest will be accrued
+	if (monthly_payment > Sum) // checking the availability to pay arrears before interest will be accrued
 	{
-		cout << "Кредит выплачен первым платежом (т.к. проценты начисляются после 1го месяца, а m > S).\nОпределить процент, под который выдана ссуда, невозможно.\n";
+		cout << "Кредит выплачен первым платежом (т.к. проценты начисляются после 1го месяца, а m > Sum).\nОпределить процент, под который выдан кредит, невозможно.\n";
 	}
-	else if (S / (n * 12) == m) // condition for percentage == 0
+	else if (Sum / (years * 12) == monthly_payment) // condition for percentage == 0
 	{
 		cout << "Кредит выдан под нулевой процент.\n";
 	}
-	else if (S / (n * 12) > m) // condition for percentage < 0
+	else if (Sum / (years * 12) > monthly_payment) // condition for percentage < 0
 	{
-		int iter = 0;
-		step = 1; // доработать шаг начальное значение
-		r = 1; // доработать r начальное значение
-		m_guess = S * r * pow((1 + r), n) / (12 * (pow((1 + r), n) - 1));
-		while (fabs(m_guess - m) >= error)
-		{
-			iter += 1;
-			printf("m_guess = %lf\t\t r = %lf\t\t iter = %d\t\t\n", m_guess, r, iter);
-			if (m_guess > m)
-			{
-				r-= step;
-				step /= 2;
-			}
-			if (2 * m_guess < m)
-			{
-				step *= 2;
-			}
-			r += step;
-			m_guess = S * r * pow((1 + r), n) / (12 * (pow((1 + r), n) - 1));
-		}
-		//cout << "Отрицательный результат: Cумма величиной " << S << ", которая гасится месячными выплатами величиной " << m << " в течение " << n << " лет, выдана под процент " << r * 100 << " (с точностью до" << error << ").\n" << endl;
-		printf("\nm_guess = %lf\t\t r = %lf\t\t iter = %d\t\t\n", m_guess, r, iter);
-		printf("\nОтрицательный результат: Cумма величиной %.2lf, которая гасится месячными выплатами величиной %.2lf в течение %.2lf лет, выдана под процент %lf (с погрешностью +- %lf).\n\n", S, m, n, r * 100, error);
+		int iteration = 0;
+		double fractional_percent = (monthly_payment * years * 12 - Sum) / Sum;
+		printf("\nОтрицательный результат: Cумма величиной %.2lf, которая гасится месячными выплатами величиной %.2lf в течение %.2lf лет, выдана под процент %lf.\n\n", Sum, monthly_payment, years, fractional_percent * 100);
 	}
 	else // condition for percentage > 0
 	{
 		int iter = 0;
 		step = 1;
-		r = 1;
-		m_guess = S * r * pow((1 + r), n) / (12 * (pow((1 + r), n) - 1));
-		while (fabs(m_guess - m) >= error)
+		fractional_percent = 1;
+		monthly_payment_guess = Sum * fractional_percent * pow((1 + fractional_percent), years) / (12 * (pow((1 + fractional_percent), years) - 1));
+		while (fabs(monthly_payment_guess - monthly_payment) >= error)
 		{
 			iter += 1;
-			printf("m_guess = %lf\t\t r = %lf\t\t iter = %d\t\t\n", m_guess, r, iter);
-			//cout << "m_guess = " << setw(8) << m_guess << setw(10) << " r = " << setw(15) << r << setw(10) << " iter = " << setw(5) << iter << setw(10) << endl;
-			if (m_guess > m)
+			printf("m_guess = %lf\t\t r = %lf\t\t iter = %d\t\t\n", monthly_payment_guess, fractional_percent, iter);
+			if (monthly_payment_guess > monthly_payment)
 			{
-				r -= step;
+				fractional_percent -= step;
 				step /= 2;
 			}
-			if (2 * m_guess < m)
+			if (2 * monthly_payment_guess < monthly_payment)
 			{
 				step *= 2;
 			}
-			r += step;
-			m_guess = S * r * pow((1 + r), n) / (12 * (pow((1 + r), n) - 1));
+			fractional_percent += step;
+			monthly_payment_guess = Sum * fractional_percent * pow((1 + fractional_percent), years) / (12 * (pow((1 + fractional_percent), years) - 1));
 		}
-		//cout << "\nm_guess = " << m_guess << "\t\t" << " r = " << r << "\t\t" << " iter = " << iter << "\t\t" << endl << endl;
-		//cout << "Положительный результат: Cумма величиной "<< S << ", которая гасится месячными выплатами величиной " << m << " в течение " << n  << " лет, выдана под процент " << r * 100 << " (с погрешностью +- " << error << ").\n" << endl;
-		printf("\nm_guess = %lf\t\t r = %lf\t\t iter = %d\t\t\n", m_guess, r, iter);
-		printf("\nПоложительный результат: Cумма величиной %.2lf, которая гасится месячными выплатами величиной %.2lf в течение %.2lf лет, выдана под процент %lf (с погрешностью +- %lf).\n\n", S, m, n, r * 100, error);
+		printf("\nm_guess = %lf\t\t r = %lf\t\t iter = %d\t\t\n", monthly_payment_guess, fractional_percent, iter);
+		printf("\nПоложительный результат: Cумма величиной %.2lf, которая гасится месячными выплатами величиной %.2lf в течение %.2lf лет, выдана под процент %lf (с погрешностью +- %lf).\n\n", Sum, monthly_payment, years, fractional_percent * 100, error);
 	}
 }
 
